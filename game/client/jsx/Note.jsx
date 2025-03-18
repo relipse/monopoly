c9c26e71c5d57b21a81588daf59c1d6e439ce09f
James Kinsman <relipse@gmail.com>
2025-03-18 09:24:39 -0500

    quick-commit: money colors fix

--- game/client/jsx/Note.jsx
+++ game/client/jsx/Note.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Note extends React.Component {
     render() {
         const value = this.props.value;
-        return (<div data-value="{value}" className={"note v" + value}>
+        return (<div className={"note v" + value}>
             ${value}
         </div>)
     }
