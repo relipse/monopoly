195a861e177e665dbf0f3b6a408ae48f0adf4796
James Kinsman <relipse@gmail.com>
2025-03-18 11:56:00 -0500

    Revert "quick-commit: chatgpt show money stuff toggle"
    This reverts commit 657b4767f21e1f61156d75334836325a213077f9.

--- game/client/jsx/Note.jsx
+++ game/client/jsx/Note.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 export default class Note extends React.Component {
     render() {
-        const { value, showMoneyText } = this.props;
-        return (
-            <div className={"note v" + value}>
-                {showMoneyText && `$${value}`}
-            </div>
-        );
+        const value = this.props.value;
+        return (<div className={"note v" + value}>
+            ${value}
+        </div>)
     }
 }
--- game/client/jsx/Settings.jsx
+++ game/client/jsx/Settings.jsx
@@ -6,15 +6,8 @@ import {faCog} from '@fortawesome/free-solid-svg-icons';
 
 export default class Settings extends React.Component {
     state = {
-        showSettings: false,
-        showMoneyText: false
+        showSettings: false
     }
-
-    toggleMoneyText = () => {
-        this.setState({ showMoneyText: !this.state.showMoneyText });
-        this.props.toggleMoneyText(!this.state.showMoneyText);  // Pass to parent
-    }
-
     saveGame = () => {
 
         const now = new Date();
@@ -75,9 +68,6 @@ export default class Settings extends React.Component {
             {this.state.showSettings && <ul>
                 <li onClick={this.saveGame}>Save game</li>
                 <li onClick={this.loadGame}>Load game</li>
-                <li onClick={this.toggleMoneyText}>
-                    {this.state.showMoneyText ? 'Hide' : 'Show'} Text Over Money
-                </li>
                 <li onClick={this.props.showHelp}>Show help dialog</li>
             </ul>
             }
